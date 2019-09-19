ad141e46ff6c8a537971970b566896d7c7f4e552
hasi-b <hasib.hasan9@gmail.com>
2019-09-19 23:41:17 +0600

    Create StudentTableDeletion.php

---
+++ StudentTableDeletion.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+$con=mysqli_connect('localhost','root','');
+mysqli_select_db($con,'rlibrary' );
+$StudentNo= $_POST['student_no'];
+$StudentName = $_POST['student_name'];
+
+
+
+
+$s= " DELETE FROM student WHERE `student`.`stud_no` = '$StudentNo';" ;
+
+$result=mysqli_query($con,$s);
+
+$Error=mysqli_error($con);
+if(empty($Error)){
+    header('location:TableStudentShow.php');
+
+}
+else{
+    echo("Error Description: ".mysqli_error($con));
+    
+}
+
+
+?>
