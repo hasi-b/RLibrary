acf947f92f18925e320faf0e2bd9d97ab40368d7
hasi-b <hasib.hasan9@gmail.com>
2019-09-19 13:41:26 +0600

    Update TableStudentShow.php

--- TableStudentShow.php
+++ TableStudentShow.php
@@ -16,16 +16,42 @@ $result=mysqli_query($con,$s);
         Student Data
         </title>
     </head>
+    <style>
+        body{
+
+            background:Seashell;
+        }
+        table{
+            width:800px;
+            margin:auto;
+            text-align:center;
+            table-layout:fixed;
+        }
+        table,tr,th,td{
+            padding:20px;
+            color:white;
+            border:1px solid #080808;
+            border-collapse:collapse;
+            font-size:18px;
+            font-family:Arial;
+            background:linear-gradient(top,#3c3c3c 0%,#222222 100%);
+            background:-webkit-linear-gradient(top,#3c3c3c 0%,#222222 100%);
+        }
+        
+        
+        
+    </style>
     <body>
         
         <table>
             <tr>
+                <th colspan="2"><h1>Student Table</h1></th>
+            <tr>
+            <tr>
                 <th> Student No</th>
                 <th> Student Name</th>
             </tr>
-            <tr>
-                <td></td>
-            </tr>
+         
             <?php
             while ($row=mysqli_fetch_assoc($result)) {
                 ?>
