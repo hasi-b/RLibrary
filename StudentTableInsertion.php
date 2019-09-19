2321595fa57d1dea6dbffdf59e395847e36a9a9c
hasi-b <hasib.hasan9@gmail.com>
2019-09-19 21:21:35 +0600

    Create StudentTableInsertion.php

---
+++ StudentTableInsertion.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+$con=mysqli_connect('localhost','root','');
+mysqli_select_db($con,'rlibrary' );
+$StudentNo= $_POST['student_no'];
+$StudentName = $_POST['student_name'];
+
+
+
+
+$s= " INSERT INTO student (`stud_name`, `stud_no`) VALUES ('$StudentName','StudentNo')" ;
+
+$result=mysqli_query($con,$s);
+
+$Error=mysqli_error($con);
+if(empty($Error)){
+    header('location:TableStudentShow.php');
+
+}
+else{
+    echo("Error Description: ".mysqli_error($con));
+    echo "<br>";
+    
+}
+
+
+?>
