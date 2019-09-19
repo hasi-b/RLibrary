baa425ee74376a109713e6585923ae5fd94a6ac7
hasi-b <hasib.hasan9@gmail.com>
2019-09-19 17:29:53 +0600

    Create BookTableInsertion.php

---
+++ BookTableInsertion.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$con=mysqli_connect('localhost','root','');
+mysqli_select_db($con,'rlibrary' );
+$BookNo= $_POST['book_no'];
+$Author = $_POST['author'];
+$BookName= $_POST['book_name'];
+
+
+
+$s= " INSERT INTO book (`book_no`, `book_name`, `author`) VALUES ('$BookNo','$BookName', '$Author')" ;
+
+$result=mysqli_query($con,$s);
+$num = mysqli_num_rows($result);
+$Error=mysqli_error($con);
+if(empty($Error)){
+    header('location:TableBookShow.php');
+
+}
+else{
+    echo("Error Description: ".mysqli_error($con));
+}
+
+
+?>
