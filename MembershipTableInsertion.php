93dd574d6f90cb3e52b452a4d09c72bb86c304e8
hasi-b <hasib.hasan9@gmail.com>
2019-09-19 17:38:39 +0600

    Create MembershipTableInsertion.php

---
+++ MembershipTableInsertion.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$con=mysqli_connect('localhost','root','');
+mysqli_select_db($con,'rlibrary' );
+$StudentNo= $_POST['student_no'];
+$MembershipNo = $_POST['membership_no'];
+
+
+
+
+$s= " INSERT INTO membership (`mem_no`, `stud_no`) VALUES ('$StudentNo','$MembershipNo')" ;
+
+$result=mysqli_query($con,$s);
+
+$Error=mysqli_error($con);
+if(empty($Error)){
+    header('location:TableMembershipShow.php');
+
+}
+else{
+    echo("Error Description: ".mysqli_error($con));
+}
+
+
+?>
